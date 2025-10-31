69cb8d72b2b0c812425adf7627b92c00845b4e78
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-31 16:22:50 +0700

    memodifikasi elevated dengan memberi ukuran

--- app/src/main/java/com/example/questuserinput_042/TextEdit.kt
+++ app/src/main/java/com/example/questuserinput_042/TextEdit.kt
@@ -121,6 +121,9 @@ fun FormDataDiri(modifier: Modifier) {
             ElevatedCard(
                 elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
                 colors = CardDefaults.cardColors(containerColor = Color.DarkGray),
-
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(300.dp)
+            )
     }
 }
