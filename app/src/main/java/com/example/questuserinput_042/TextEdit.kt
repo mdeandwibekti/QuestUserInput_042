559133e2dd0ce040c3126e8751a942e920f21f53
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-31 16:15:37 +0700

    membuat outlined untuk text nama dan memberi shape agar column menjadi tidak kotak

--- app/src/main/java/com/example/questuserinput_042/TextEdit.kt
+++ app/src/main/java/com/example/questuserinput_042/TextEdit.kt
@@ -49,6 +49,17 @@ fun FormDataDiri(modifier: Modifier) {
             modifier = Modifier.padding(top = 50.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        )
-    }
+        ) {
+            OutlinedTextField(
+                value = textNama,
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {
+                    textNama = it
+                }
+            )
+
 }
