b0b582b970cc90d7d3566c04d049f95c73fd98d8
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-31 16:14:10 +0700

    membuat colums agar berada diatas dan membuat jarak

--- app/src/main/java/com/example/questuserinput_042/TextEdit.kt
+++ app/src/main/java/com/example/questuserinput_042/TextEdit.kt
@@ -45,5 +45,10 @@ fun FormDataDiri(modifier: Modifier) {
 
         val gender: List<String> = listOf("Laki-laki", "Perempuan")
 
-
+        Column(
+            modifier = Modifier.padding(top = 50.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+    }
 }
